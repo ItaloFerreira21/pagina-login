5908427009b56725b257538b1f9c46d4508465b4
italoFerreira21 <duduu21q@gmail.com>
2024-03-21 17:28:04 -0300

    estilo adicionada ao layout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
+import { FormGroup,FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    LoginLayoutComponent
+    LoginLayoutComponent, ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup; //a exclamação indica que sera declarado em algum momento
 
+  constructor(){
+    this.loginForm = new FormGroup({
+      username: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required, Validators.minLength(6)]) //minimo de 6 caracters na senha
+    })
+  }
 }
